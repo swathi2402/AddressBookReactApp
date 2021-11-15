e08f7953b54d5698fc20afdb164b105f498644ad
Swathi <swathi.h@perfios.com>
2021-11-15 20:58:46 +0530

    ADDED: Method to get all the contacts

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -3,13 +3,34 @@ import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.png';
 import addIcon from '../../assets/add-24px.svg';
 import './home.scss';
+import AddressBookService from "../../service/AddressBook-service";
+
 class Home extends React.Component {
+
+    constructor() {
+        super()
+        this.state = {
+            contactArray: []
+
+        }
+    }
+
+    addressBookService = new AddressBookService();
+
+    getAllContacts = () => {
+        this.addressBookService.getAllContacts().then(data => {
+            console.log("Data after get ", data.data);
+            this.setState({ contactArray: data.data })
+        }).catch(error => {
+            console.log("Error after ", error);
+        })
+    }
 
     render() {
         return (
             <div className="main">
                 <header className="header-content header">
-                <div className="logo-content">
+                    <div className="logo-content">
                         <img src={logo} alt="" />
                         <div>
                             <span className="address-text">ADDRESS</span> <br />
@@ -25,7 +46,7 @@ class Home extends React.Component {
                         <Link to="/add" className="add-button flex-row-center"><img src={addIcon} alt="" />Add Person</Link>
                     </div>
                     <div className="table-main">
-                        {/* <Display employeeArray={ } /> */}
+                        <Display contactArray={this.contactArray} />
                     </div>
                 </div>
             </div>
