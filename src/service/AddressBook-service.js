40edadfc2246e14110e4baac6b4d2a22843cc4cf
Swathi <swathi.h@perfios.com>
2021-11-15 20:47:25 +0530

    ADDED: Get call to JSON server using axios

--- src/service/AddressBook-service.js
+++ src/service/AddressBook-service.js
@@ -3,8 +3,13 @@ import AxiosService from './AxiousService';
 
 export default class AddressBookService {
     baseUrl = config.baseUrl;
+
     addContact(data) {
         console.log("URL", this.baseUrl);
         return AxiosService.postService(`${this.baseUrl}AddressBook`, data);
+    }
+
+    getAllContacts() {
+        return AxiosService.getService(`${this.baseUrl}AddressBook`);
     }
 } 
