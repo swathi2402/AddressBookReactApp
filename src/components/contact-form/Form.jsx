be371515840d4d3b1b1971fbb7df9f33486248b8
Swathi <swathi.h@perfios.com>
2021-11-18 21:10:01 +0530

    ADDED: Disabling button on invalid data and empty form

--- src/components/contact-form/Form.jsx
+++ src/components/contact-form/Form.jsx
@@ -28,6 +28,8 @@ const Form = (props) => {
 
     const [formValue, setForm] = useState(initialValue);
     const [displayMeassage, setDisplayMessage] = useState("");
+    const [disable, setDisable] = useState(true);
+    const [resetDisable, setResetDisable] = useState(true);
     const params = useParams();
 
     const addressbookService = new AddressBookService();
@@ -38,6 +40,22 @@ const Form = (props) => {
         }
     }, []);
 
+    useEffect(() => {
+        if (!validData()) {
+            setDisable(false);
+        } else {
+            setDisable(true);
+        }
+    }, [disable]);
+
+    useEffect(() => {
+        if (emptyForm()) {
+            setResetDisable(true);
+        } else {
+            setResetDisable(false);
+        }
+    }, [resetDisable]);
+    
     const getDataById = (id) => {
         addressbookService.getContact(id).then((data) => {
             console.log("Data is ", data.data.data);
@@ -47,18 +65,29 @@ const Form = (props) => {
             console.log("Error is ", error);
         });
     };
-
+    
     const setData = (object) => {
         setForm({
             ...formValue, ...object, isUpdate: true,
         });
     };
-
+    
     const changeValue = (event) => {
+        setDisable(!disable);
+        setResetDisable(!resetDisable);
         setForm({ ...formValue, [event.target.name]: event.target.value })
     }
+    
+    const emptyForm = () => {
+        let isEmpty = false;
+        
+        if (formValue.name === "" && formValue.state === "" && formValue.city === "" && formValue.address === "" && formValue.zip === "" && formValue.phoneNumber === "") {
+            isEmpty = true;
+        }
+        return isEmpty;
+    }
 
-    const validData = async () => {
+    const validData = () => {
         let isError = false;
         let error = {
             name: '',
@@ -68,37 +97,56 @@ const Form = (props) => {
             zip: '',
             phoneNumber: ''
         }
-        if (!formValue.name.match('^[A-Z]{1}[a-zA_Z\\s]{2,}$')) {
+        if (formValue.name !== "" && !formValue.name.match('^[A-Z]{1}[a-zA_Z\\s]{2,}$')) {
             error.name = 'Name is Invalid!'
             isError = true;
         }
-        if (formValue.state.length < 1) {
+        if (formValue.state !== "" && formValue.state.length < 1) {
             error.state = 'State is required field!'
             isError = true;
         }
-        if (formValue.city.length < 1) {
+        if (formValue.city !== "" && formValue.city.length < 1) {
             error.city = 'City is required field!'
             isError = true;
         }
-        if (!formValue.address.match('([a-zA-Z]{3,}\\s?){2,}$')) {
+        if (formValue.address !== "" && !formValue.address.match('([a-zA-Z]{3,}\\s?){2,}$')) {
             error.address = 'Address is Invalid!'
             isError = true;
         }
-        if (!formValue.zip.match('^[0-9]{3}\\s{0,1}[0-9]{3}$')) {
+        if (formValue.zip !== "" && !formValue.zip.match('^[0-9]{3}\\s{0,1}[0-9]{3}$')) {
             error.zip = 'Zip code is Invalid!'
             isError = true;
         }
-        if (!formValue.phoneNumber.match('^([+]?[1-9][0-9])?[0-9]{10}$')) {
+        if (formValue.phoneNumber !== "" && !formValue.phoneNumber.match('^([+]?[1-9][0-9])?[0-9]{10}$')) {
             error.phoneNumber = 'Phone Number Invalid!'
             isError = true;
         }
-        await setForm({ ...formValue, error: error })
+
+        if (formValue.name === "") {
+            isError = true;
+        }
+        if (formValue.state === "") {
+            isError = true;
+        }
+        if (formValue.city === "") {
+            isError = true;
+        }
+        if (formValue.address === "") {
+            isError = true;
+        }
+        if (formValue.zip === "") {
+            isError = true;
+        }
+        if (formValue.phoneNumber === "") {
+            isError = true;
+        }
+        setForm({ ...formValue, error: error })
         return isError;
     }
 
     const save = async (event) => {
         event.preventDefault();
-        if (await validData()) {
+        if (validData()) {
             console.log('error', formValue);
             return;
         }
@@ -165,7 +213,7 @@ const Form = (props) => {
                 </div>
             </header>
             <div className="form-content">
-                <form className="form" action="#" onSubmit={save}>
+                <form name="person" className="form" action="#" onSubmit={save}>
                     <div className="form-head">
                         <h1 className="form-head-title">Person Address Form</h1>
                         <Link to="">
@@ -193,7 +241,7 @@ const Form = (props) => {
                         <div>
                             <label className="label text" htmlFor="city">City</label>
                             <select value={formValue.city} onChange={changeValue} id="city" name="city">
-                                <option value="" disabled>Select City</option>
+                                <option>Select City</option>
                                 <option value="Bangalore">Bangalore</option>
                                 <option value="Chennai">Chennai</option>
                                 <option value="Hyderabad">Hyderabad</option>
@@ -206,7 +254,7 @@ const Form = (props) => {
                         <div>
                             <label className="label text" htmlFor="state">State</label>
                             <select value={formValue.state} onChange={changeValue} id="state" name="state">
-                                <option value="" disabled>Select State</option>
+                                <option>Select State</option>
                                 <option value="Karnataka">Karnataka</option>
                                 <option value="Telengana">Telengana</option>
                                 <option value="Tamilnadu">Tamil Nadu</option>
@@ -220,8 +268,8 @@ const Form = (props) => {
                         </div>
                     </div>
                     <div className="buttonParent">
-                        <button type="submit" className="button submitButton" id="addButton" >{formValue.isUpdate ? 'Update' : 'Add'}</button>
-                        <button type="reset" onClick={reset} className="button resetButton">Reset</button>
+                        <button type="submit" className="button submitButton" id="addButton" disabled={disable}>{formValue.isUpdate ? 'Update' : 'Add'}</button>
+                        <button type="reset" onClick={reset} className="button resetButton" disabled={resetDisable}>Reset</button>
                     </div>
                     <div className="displaymessage">
                         {displayMeassage}
